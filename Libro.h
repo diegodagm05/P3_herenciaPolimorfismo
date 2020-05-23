af8ffe9849b681368d9c3e810f91c970cc135355
diegodagm05 <61124350+diegodagm05@users.noreply.github.com>
2020-05-23 16:34:48 -0500

    class Libro - clase terminada con sus métodos

---
+++ Libro.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "Material.h"
+
+using namespace std;
+
+class Libro : public Material
+{
+    public:
+        Libro();
+        Libro(int, string, int, string);
+        
+        void setNumPag(int np) {numPag = np;};
+        void setAutor(string a) { autor = a;};
+        
+        int getNumPag() {return numPag;};
+        string getAutor() {return autor;};
+        
+        //MÃ©todos abstractos
+        
+    private:
+        int numPag;
+        string autor;
+};
+
+Libro::Libro() : Material()
+{
+    numPag = 0;
+    autor = "N/A";
+}
+
+Libro::Libro(int idm, string t, int numPag, string autor) : Material(idm, t)
+{
+    this->numPag = numPag;
+    this->autor = autor;
+}
+void Libro::muestraDatos()
+{
+    cout<<"Libro"<<idMaterial<<titulo<<numPag<<autor<<endl;
+}
+
+int Libro::cantidadDiasPrestamo()
+{
+    return 7;
+}
