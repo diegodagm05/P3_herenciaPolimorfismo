cfa0f5bb85de6ed9df49df2f62ad841b4fc92c79
diegodagm05 <61124350+diegodagm05@users.noreply.github.com>
2020-05-23 16:36:46 -0500

    class Software - clase terminada con sus métodos

---
+++ Software.h
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class Software
+{
+    public:
+        Software();
+        Software(int, string, int, string);
+        
+        void setVersion(int v) {version = v;};
+        void setSO(string s) {SO = s;};
+        
+        int getVersion() {return version;};
+        string getSO() {return SO;};
+        
+        //Métodos abstractos
+        
+    private:
+        int version;
+        string SO;
+};
+
+Software::Software() : Material()
+{
+    version = 0;
+    SO = "N/A";
+}
+
+Software::Software(int idm, string t, int version, string SO) : Material(idm, t)
+{
+    this->version = version;
+    this->SO = SO;
+}
+
+void Software::muestraDatos()
+{
+    cout<<"Software"<<idMaterial<<titulo<<version<<SO<<endl;
+}
+
+int Software::cantidadDiasPrestamo()
+{
+    return 1;
+}
